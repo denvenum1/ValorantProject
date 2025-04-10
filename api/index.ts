97912ebaaab301f7d597fdca704de9663653008a
Ali Soujaa <ali.soujaa@ap.be>
2025-04-10 12:12:21 +0200

    fixeded

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,20 @@
 import express from "express";
-import router from "../router/routers";
-import { connect } from '../database';
+import router from "../routes/routers";
+import { connect } from "../database";
 import { errorHandler } from "../middleware/middleware";
 import session from "../middleware/session";
 import createServerlessHandler from "@vendia/serverless-express";
-
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(session);
 app.use("/", router);
 
+// Error handlers
 app.use(errorHandler(404, "The page you were trying to find does not exist"))
    .use(errorHandler(500, "Internal server error. Please try again later."))
    .use(errorHandler(403, "Forbidden. Access denied."))
@@ -24,4 +23,4 @@ app.use(errorHandler(404, "The page you were trying to find does not exist"))
 
 await connect();
 
-export default createServerlessHandler({ app }); 
+export default createServerlessHandler({ app });
